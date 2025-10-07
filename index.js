9b2cbad28ec51ad6c8edd96255f98c5265bf0310
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-10-07 00:14:56 -0500

    first object

---
+++ index.js
@@ -0,0 +1,11 @@
+var book ={
+    title: "To kill a Mockingbird",
+    author: "Harper Lee",
+    page:324
+    read: function(){
+        console.log ("Currently reading " + book.title + " by" book.author + ".")
+    }
+}
+
+
+fu
