da574d8daac3aa41d580a953a3aa22f15f48ef70
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-10-29 20:33:00 -0500

    corrections

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var book ={
     title: "To kill a Mockingbird",
     author: "Harper Lee",
-    page:324
+    page:324,
     read: function(){
-        console.log ("Currently reading " + book.title + " by" book.author + ".")
+        console.log ("Currently reading " + book.title + " by" + book.author + ".")
     }
 }
 
@@ -21,3 +21,16 @@ const animal2 = new Animal("lion", "Leo", "roar")
 
 animal1.makeSound()
 animal2.makeSound()
+
+function Student(name,grade, subject){
+    this.name = name
+    this.grade = grade
+    this.subject = subject
+    this.inroduce(){
+        console.log (`Hi, I am ${this.name}, studying ${subject}, in grade ${grade}`)
+        this. finishesSchoolYear(){
+            grade +=1 grade < 13
+            if grade === 12 console.log(`Congratulations ${name} has graduated`) &
+        }
+    }    
+} 
